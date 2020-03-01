6565b28e85b1e54e01bbc57cecd8c48cf20a78d8
Daniel Starns <danielstarns@hotmail.com>
2020-03-01 17:11:18 +0000

    Refactor: remove gql tag and point schema to files

--- src/graphql/nodes/Article/Article.js
+++ src/graphql/nodes/Article/Article.js
@@ -1,5 +1,5 @@
 const { GraphQLNode } = require("idio-graphql");
-const gql = require("graphql-tag");
+const path = require("path");
 const nodes = require("./nodes/index.js");
 const Query = require("./Query/index.js");
 const Mutation = require("./Mutation/index.js");
@@ -7,122 +7,7 @@ const Mutation = require("./Mutation/index.js");
 const Article = new GraphQLNode({
     name: "Article",
     nodes,
-    typeDefs: gql`
-        type Article {
-            id: ID!
-
-            author: User!
-            body: String!
-            comments: [Comment!]!
-            description: String!
-            favoritesCount: Int!
-            slug: String!
-            tagList: [String!]!
-            title: String!
-            viewerHasFavorited: Boolean!
-
-            updatedAt: DateTime!
-            createdAt: DateTime!
-        }
-
-        type ArticleEdge {
-            cursor: String!
-            node: Article
-        }
-
-        type ArticleConnection {
-            edges: [ArticleEdge]
-            pageInfo: PageInfo!
-        }
-
-        type CreateArticlePayload {
-            article: Article
-            clientMutationId: String
-            errors: [UserError!]!
-        }
-
-        type DeleteArticlePayload {
-            article: Article!
-            clientMutationId: String
-        }
-
-        input DeleteArticleInput {
-            id: ID!
-            clientMutationId: String
-        }
-
-        input FavoriteArticleInput {
-            id: ID!
-            clientMutationId: String
-        }
-
-        type FavoriteArticlePayload {
-            article: Article
-            clientMutationId: String
-        }
-
-        input UpdateArticleInput {
-            id: ID!
-            title: String!
-            description: String!
-            body: String!
-            tagList: [String!]!
-            clientMutationId: String
-        }
-
-        type UpdateArticlePayload {
-            article: Article
-            clientMutationId: String
-            errors: [UserError!]!
-        }
-
-        input UnfavoriteArticleInput {
-            id: ID!
-            clientMutationId: String
-        }
-
-        type UnfavoriteArticlePayload {
-            article: Article
-            clientMutationId: String
-        }
-
-        input CreateArticleInput {
-            title: String!
-            description: String!
-            body: String!
-            tagList: [String!]!
-            clientMutationId: String
-        }
-
-        type Query {
-            article(slug: String!): Article
-
-            articles(
-                first: Int
-                after: String
-                last: Int
-                before: String
-                tag: String
-                authoredBy: String
-                favoritedBy: String
-            ): ArticleConnection!
-        }
-
-        type Mutation {
-            createArticle(input: CreateArticleInput!): CreateArticlePayload
-            deleteArticle(input: DeleteArticleInput!): DeleteArticlePayload
-
-            favoriteArticle(
-                input: FavoriteArticleInput!
-            ): FavoriteArticlePayload
-
-            unfavoriteArticle(
-                input: UnfavoriteArticleInput!
-            ): UnfavoriteArticlePayload
-
-            updateArticle(input: UpdateArticleInput!): UpdateArticlePayload
-        }
-    `,
+    typeDefs: path.join(__dirname, "./Article.gql"),
     resolvers: {
         Query,
         Mutation
