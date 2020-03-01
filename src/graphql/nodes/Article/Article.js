3527f4207c33ee43fb8d0ac32fbf9fb90b5f64ef
Daniel Starns <danielstarns@hotmail.com>
2020-03-01 15:33:22 +0000

    Feat: migrate graphql schema and update model for Article

--- src/graphql/nodes/Article/Article.js
+++ src/graphql/nodes/Article/Article.js
@@ -5,17 +5,116 @@ const Article = new GraphQLNode({
     name: "Article",
     typeDefs: gql`
         type Article {
-            _id: ID!
-            createdAt: String!
-            updatedAt: String!
-            slug: String
-            title: String
-            description: String
-            body: String
-            favoritesCount: Int
-            comments: [Comment]
-            tagList: [String]
-            author: User
+            id: ID!
+            author: User!
+            body: String!
+            comments: [Comment!]!
+            createdAt: DateTime!
+            description: String!
+            favoritesCount: Int!
+            slug: String!
+            tagList: [String!]!
+            title: String!
+            updatedAt: DateTime!
+            viewerHasFavorited: Boolean!
+        }
+
+        type ArticleEdge {
+            cursor: String!
+            node: Article
+        }
+
+        type ArticleConnection {
+            edges: [ArticleEdge]
+            pageInfo: PageInfo!
+        }
+
+        type CreateArticlePayload {
+            article: Article
+            clientMutationId: String
+            errors: [UserError!]!
+        }
+
+        type DeleteArticlePayload {
+            article: Article!
+            clientMutationId: String
+        }
+
+        input DeleteArticleInput {
+            id: ID!
+            clientMutationId: String
+        }
+
+        input FavoriteArticleInput {
+            id: ID!
+            clientMutationId: String
+        }
+
+        type FavoriteArticlePayload {
+            article: Article
+            clientMutationId: String
+        }
+
+        input UpdateArticleInput {
+            id: ID!
+            title: String!
+            description: String!
+            body: String!
+            tagList: [String!]!
+            clientMutationId: String
+        }
+
+        type UpdateArticlePayload {
+            article: Article
+            clientMutationId: String
+            errors: [UserError!]!
+        }
+
+        input UnfavoriteArticleInput {
+            id: ID!
+            clientMutationId: String
+        }
+
+        type UnfavoriteArticlePayload {
+            article: Article
+            clientMutationId: String
+        }
+
+        input CreateArticleInput {
+            title: String!
+            description: String!
+            body: String!
+            tagList: [String!]!
+            clientMutationId: String
+        }
+
+        type Query {
+            article(slug: String!): Article
+
+            articles(
+                first: Int
+                after: String
+                last: Int
+                before: String
+                tag: String
+                authoredBy: String
+                favoritedBy: String
+            ): ArticleConnection!
+        }
+
+        type Mutation {
+            createArticle(input: CreateArticleInput!): CreateArticlePayload
+            deleteArticle(input: DeleteArticleInput!): DeleteArticlePayload
+
+            favoriteArticle(
+                input: FavoriteArticleInput!
+            ): FavoriteArticlePayload
+
+            unfavoriteArticle(
+                input: UnfavoriteArticleInput!
+            ): UnfavoriteArticlePayload
+
+            updateArticle(input: UpdateArticleInput!): UpdateArticlePayload
         }
     `
 });
--- src/models/Article.js
+++ src/models/Article.js
@@ -1,64 +1,17 @@
 const mongoose = require("mongoose");
-const uniqueValidator = require("mongoose-unique-validator");
-const slug = require("slug");
-const User = require("./User.js");
 
 const Article = new mongoose.Schema(
     {
-        slug: { type: String, lowercase: true, unique: true },
+        slug: String,
         title: String,
         description: String,
         body: String,
         favoritesCount: { type: Number, default: 0 },
         comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
-        tagList: [{ type: String }],
+        tagList: [String],
         author: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
     },
     { timestamps: true }
 );
-
-Article.plugin(uniqueValidator, { message: "is already taken" });
-
-Article.pre("validate", function(next) {
-    if (!this.slug) {
-        this.slugify();
-    }
-
-    next();
-});
-
-Article.methods.slugify = function() {
-    this.slug = `${slug(this.title)}-${(
-        (Math.random() * Math.pow(36, 6)) |
-        0
-    ).toString(36)}`;
-};
-
-Article.methods.updateFavoriteCount = function() {
-    const article = this;
-
-    return User.count({ favorites: { $in: [article._id] } }).then(function(
-        count
-    ) {
-        article.favoritesCount = count;
-
-        return article.save();
-    });
-};
-
-Article.methods.toJSONFor = function(user) {
-    return {
-        slug: this.slug,
-        title: this.title,
-        description: this.description,
-        body: this.body,
-        createdAt: this.createdAt,
-        updatedAt: this.updatedAt,
-        tagList: this.tagList,
-        favorited: user ? user.isFavorite(this._id) : false,
-        favoritesCount: this.favoritesCount,
-        author: this.author.toProfileJSONFor(user)
-    };
-};
 
 module.exports = mongoose.model("Article", Article);
