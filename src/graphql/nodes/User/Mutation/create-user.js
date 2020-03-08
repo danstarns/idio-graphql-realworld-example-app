3a771410f68d78a47b6be6c3b0f8b85598148a8b
Daniel Starns <danielstarns@hotmail.com>
2020-03-08 10:30:22 +0000

    Feat: add create user logic

--- src/graphql/nodes/User/Mutation/create-user.js
+++ src/graphql/nodes/User/Mutation/create-user.js
@@ -1,3 +1,28 @@
-function createUser() {}
+const { User } = require("../../../../models/User.js");
+
+async function createUser(root, args) {
+    const {
+        input: { username, email, password }
+    } = args;
+
+    const CreateUserPayload = { errors: [], user: null };
+
+    if (await User.findOne({ username })) {
+        return {
+            ...CreateUserPayload,
+            errors: [
+                {
+                    message: "Email has already been taken",
+                    path: new Error().stack
+                }
+            ]
+        };
+    }
+
+    return {
+        ...CreateUserPayload,
+        user: await User.create({ username, email, password })
+    };
+}
 
 module.exports = createUser;
