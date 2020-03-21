036c1fc0b119e8a09d486603a6d5f4fd7f238ad3
Daniel Starns <danielstarns@hotmail.com>
2020-03-21 14:08:55 +0000

    Refactor: change error logic on create user

--- src/graphql/nodes/User/Mutation/create-user.js
+++ src/graphql/nodes/User/Mutation/create-user.js
@@ -2,30 +2,28 @@ const { User } = require("../../../../models/User.js");
 const { hashPassword } = require("../../../../util/index.js");
 
 async function createUser(root, args) {
+    const CreateUserPayload = { errors: [], user: null };
     const {
         input: { username, email, password }
     } = args;
 
-    const CreateUserPayload = { errors: [], user: null };
+    try {
+        if (await User.findOne({ username })) {
+            throw new Error("Email has already been taken");
+        }
 
-    if (await User.findOne({ username })) {
+        const hash = await hashPassword(password);
+
         return {
             ...CreateUserPayload,
-            errors: [
-                {
-                    message: "Email has already been taken",
-                    path: new Error().stack
-                }
-            ]
+            user: await User.create({ username, email, password: hash })
+        };
+    } catch (error) {
+        return {
+            ...CreateUserPayload,
+            errors: error
         };
     }
-
-    const hash = await hashPassword(password);
-
-    return {
-        ...CreateUserPayload,
-        user: await User.create({ username, email, password: hash })
-    };
 }
 
 module.exports = createUser;
