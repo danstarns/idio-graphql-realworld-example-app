84c1472b9f79ae6051826b48f221ac8063503978
Daniel Starns <danielstarns@hotmail.com>
2020-03-22 18:22:21 +0000

    Feat: add follow and unfollow user resolvers

--- src/graphql/nodes/User/Mutation/follow-user.js
+++ src/graphql/nodes/User/Mutation/follow-user.js
@@ -1,3 +1,29 @@
-function followUser() {}
+const { User } = require("../../../../models/index.js");
+
+async function followUser(root, { user: userToFollow }, { user: requester }) {
+    try {
+        const user = await User.findById(userToFollow);
+
+        if (!user) {
+            throw new Error(/* user not found */);
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(
+            requester,
+            {
+                $addToSet: { following: userToFollow }
+            },
+            { new: true }
+        ).lean();
+
+        return {
+            user: updatedUser
+        };
+    } catch (error) {
+        return {
+            user: null
+        };
+    }
+}
 
 module.exports = followUser;
--- src/graphql/nodes/User/Mutation/unfollow-user.js
+++ src/graphql/nodes/User/Mutation/unfollow-user.js
@@ -1,3 +1,33 @@
-function unfollowUser() {}
+const { User } = require("../../../../models/index.js");
+
+async function unfollowUser(
+    root,
+    { user: userToUnfollow },
+    { user: requester }
+) {
+    try {
+        const user = await User.findById(userToUnfollow);
+
+        if (!user) {
+            throw new Error(/* user not found */);
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(
+            requester,
+            {
+                $pull: { following: userToUnfollow }
+            },
+            { new: true }
+        ).lean();
+
+        return {
+            user: updatedUser
+        };
+    } catch (error) {
+        return {
+            user: null
+        };
+    }
+}
 
 module.exports = unfollowUser;
