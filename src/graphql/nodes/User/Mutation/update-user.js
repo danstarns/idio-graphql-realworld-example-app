ac0b7aa951cac2a4b248112d8bca2470b1a5248f
Daniel Starns <danielstarns@hotmail.com>
2020-03-22 18:22:55 +0000

    Feat: add update user resolver

--- src/graphql/nodes/User/Mutation/update-user.js
+++ src/graphql/nodes/User/Mutation/update-user.js
@@ -1,3 +1,34 @@
-function updateUser() {}
+const { User } = require("../../../../models/index.js");
+
+async function updateUser(root, { input }, { user: requester }) {
+    const UpdateUserPayload = { errors: [], user: null };
+
+    try {
+        const existing = await User.findById(requester).lean();
+
+        if (!existing) {
+            throw new Error(`User not found`);
+        }
+
+        const updated = await User.findByIdAndUpdate(
+            existing._id,
+            {
+                ...existing,
+                ...input
+            },
+            { new: true }
+        );
+
+        return {
+            ...UpdateUserPayload,
+            user: updated
+        };
+    } catch ({ message, stack }) {
+        return {
+            ...UpdateUserPayload,
+            errors: [{ message, stack }]
+        };
+    }
+}
 
 module.exports = updateUser;
