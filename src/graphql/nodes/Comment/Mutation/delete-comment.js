3bb0e35b64a73e7a12b23ac1259499379893b300
Daniel Starns <danielstarns@hotmail.com>
2020-03-22 12:28:08 +0000

    Feat: implement delete comment resolver

--- src/graphql/nodes/Comment/Mutation/delete-comment.js
+++ src/graphql/nodes/Comment/Mutation/delete-comment.js
@@ -1,3 +1,17 @@
-function deleteComment() {}
+const { Comment } = require("../../../../models/index.js");
+
+async function deleteComment(root, { input }) {
+    try {
+        const comment = await Comment.findByIdAndRemove(input.id);
+
+        return {
+            comment
+        };
+    } catch (error) {
+        return {
+            comment: null
+        };
+    }
+}
 
 module.exports = deleteComment;
