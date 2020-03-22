264bc27d2e00e905932a251c6935f3e2c8e44a5c
Daniel Starns <danielstarns@hotmail.com>
2020-03-22 12:36:05 +0000

    Feat: implement create article

--- src/graphql/nodes/Article/Mutation/create-article.js
+++ src/graphql/nodes/Article/Mutation/create-article.js
@@ -1,3 +1,27 @@
-function createArticle() {}
+const { Article } = require("../../../../models/index.js");
+
+async function createArticle(root, { input }, { user }) {
+    const CreateArticlePayload = { article: null, errors: [] };
+
+    try {
+        const article = await Article.create({
+            author: user,
+            body: input.body,
+            description: input.description,
+            tagList: input.tagList,
+            title: input.title
+        });
+
+        return {
+            ...CreateArticlePayload,
+            article
+        };
+    } catch ({ message, stack }) {
+        return {
+            ...CreateArticlePayload,
+            errors: [{ message, path: stack }]
+        };
+    }
+}
 
 module.exports = createArticle;
