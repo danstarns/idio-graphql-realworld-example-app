f013c8a2f69f37f69e41813926d567f0aabe9cff
Daniel Starns <danielstarns@hotmail.com>
2020-03-25 18:01:54 +0000

    Fix: remove slug from article model

--- src/models/Article.js
+++ src/models/Article.js
@@ -6,7 +6,6 @@ const Article = new mongoose.Schema(
         author: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
         body: { type: String, required: true },
         description: { type: String, required: true },
-        slug: { type: String, required: true },
         tagList: [String],
         title: { type: String, required: true }
     },
