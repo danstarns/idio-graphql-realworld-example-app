7d2a8ad5d2d6e101ed4b2faba6d6d5a5dfe43fe2
Daniel Starns <danielstarns@hotmail.com>
2020-03-29 10:45:48 +0100

    Fix: imporve visability of public data when logged out.

--- src/graphql/nodes/User/Fields/followed-by-viewer.js
+++ src/graphql/nodes/User/Fields/followed-by-viewer.js
@@ -3,6 +3,10 @@ const { User } = require("../../../../models/index.js");
 async function followedByViewer(root, args, { user: userId }) {
     const user = await User.findById(userId).lean();
 
+    if (!user) {
+        return false;
+    }
+
     const userFollowers = user.following.map(x => x.toString());
 
     if (userFollowers.includes(root._id.toString())) {
