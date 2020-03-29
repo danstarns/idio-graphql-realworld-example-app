62c47b1eb5dbed1a0727ce49207e0dfd8c95514e
Daniel Starns <danielstarns@hotmail.com>
2020-03-29 12:13:47 +0100

    Feat: make Article.author required on model

--- src/models/Article.js
+++ src/models/Article.js
@@ -3,7 +3,11 @@ const mongoosePaginate = require("mongoose-paginate-v2");
 
 const Article = new mongoose.Schema(
     {
-        author: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+        author: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
         body: { type: String, required: true },
         description: { type: String, required: true },
         tagList: [String],
