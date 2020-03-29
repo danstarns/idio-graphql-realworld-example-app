84089ac40b3b8103abbea00f3a123ff06cf7a5bb
Daniel Starns <danielstarns@hotmail.com>
2020-03-29 10:28:16 +0100

    Fix: handel inconsistancy with _id and id in viewerHasFavorited

--- src/graphql/nodes/Article/Fields/viewer-has-favorited.js
+++ src/graphql/nodes/Article/Fields/viewer-has-favorited.js
@@ -1,9 +1,23 @@
+/* eslint-disable camelcase */
 const { User } = require("../../../../models/index.js");
 
-async function viewerHasFavorited({ id }, args, { user: userId }) {
+async function viewerHasFavorited({ id, _id }, args, { user: userId }) {
     const user = await User.findById(userId).lean();
 
-    if (user.favorites.articles.map(x => x.toString()).includes(id)) {
+    const articles = user.favorites.articles.map(x => x.toString());
+
+    let includesId;
+    let includes_Id;
+
+    if (id) {
+        articles.includes(id);
+    }
+
+    if (_id) {
+        includes_Id = articles.includes(_id.toString());
+    }
+
+    if (includesId || includes_Id) {
         return true;
     }
 
