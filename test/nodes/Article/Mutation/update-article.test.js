4ef745c469b279f0c01a64f4ba70bf4c6d1ec32d
Daniel Starns <danielstarns@hotmail.com>
2020-03-29 16:15:14 +0100

    Test: add updateArticle test

---
+++ test/nodes/Article/Mutation/update-article.test.js
@@ -0,0 +1,101 @@
+const { MongoMemoryServer } = require("mongodb-memory-server-global");
+const { gql } = require("apollo-server-express");
+const { expect } = require("chai");
+const mongoose = require("mongoose");
+const { User, Article } = require("../../../../src/models/index.js");
+const graphql = require("../../../graphql.js");
+
+describe("Article.Mutation.updateArticle", () => {
+    let mongo;
+
+    before(async () => {
+        mongo = new MongoMemoryServer();
+
+        const mongoUri = await mongo.getUri();
+
+        mongoose.set("useCreateIndex", true);
+        mongoose.set("useFindAndModify", false);
+
+        await mongoose.connect(mongoUri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+    });
+
+    after(async () => {
+        await mongoose.disconnect();
+
+        await mongo.stop();
+    });
+
+    afterEach(async () => {
+        const collections = await mongoose.connection.db.collections();
+
+        await Promise.all(collections.map(collection => collection.drop()));
+    });
+
+    it("should update a article", async () => {
+        const user = await User.create({
+            image: "http://cat.com",
+            username: "Tester",
+            bio: "Testing always...",
+            email: "test@test.com",
+            password: "secretHASH",
+            following: [],
+            favorites: {
+                articles: []
+            }
+        });
+
+        const article = await Article.create({
+            author: user._id,
+            body: "I love beer!",
+            description: "Me talking about beer",
+            tagList: ["beer", "bongs"],
+            title: "Beer"
+        });
+
+        const { mutate } = graphql({ user: user._id.toString() });
+
+        const UpdateArticleInput = {
+            id: article._id.toString(),
+            title: article.title,
+            description: article.description,
+            body: "I love beer soo much",
+            tagList: article.tagList
+        };
+
+        const { data, errors } = await mutate({
+            mutation: gql`
+                mutation($UpdateArticleInput: UpdateArticleInput!) {
+                    updateArticle(input: $UpdateArticleInput) {
+                        article {
+                            id
+                            body
+                            title
+                        }
+                    }
+                }
+            `,
+            variables: {
+                UpdateArticleInput
+            }
+        });
+
+        expect(errors).to.equal(undefined);
+
+        expect(data.updateArticle).to.be.a("object");
+
+        expect(data.updateArticle.article)
+            .to.have.property("id")
+            .to.equal(article._id.toString());
+
+        expect(data.updateArticle.article)
+            .to.have.property("title")
+            .to.equal(article.title);
+
+        expect(data.updateArticle.article)
+            .to.have.property("body")
+            .to.equal(UpdateArticleInput.body);
+    });
+});
